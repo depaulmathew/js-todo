578de3fe9a9d715811ff6ce1e6c79d34b168f110
David Sounthiraraj <sdbrain@gmail.com>
2023-04-20 17:42:23 -0500

    updated with class structure..

--- main.js
+++ main.js
@@ -1,23 +1,65 @@
+// initialize the DB
+const createQuery = "CREATE TABLE todo (todo_id INTEGER PRIMARY KEY, todo TEXT NOT NULL, state TEXT NOT NULL);";
+const insertQuery = "INSERT INTO todo VALUES (?, ?, ?)";
+const deleteQuery = "DELETE FROM todo WHERE todo_id = ?";
+
+// initialize the DB
+var dbAPI = new DB(createQuery);
+
 function insertTODO() {
   var text = document.getElementById("myInput").value;
-  db.run("INSERT INTO todo VALUES (?, ?, ?)", [null, text, "Done"]);
+  dbAPI.insert(insertQuery, [null, text, "Done"]);
 
-  // Boilerplate: render the table
   renderTable();
 }
 
 function deleteTODO(todoID) {
-    db.run("DELETE FROM todo WHERE todo_id = ?", [todoID]);
+    dbAPI.delete(deleteQuery, [todoID]);
     
-    // Boilerplate: render the table
     renderTable();
 }
 
 // TODO this is more involved.. e.g. how to update a specific row?
 function updateTODO() {
     var text = document.getElementById("myInput").value;
-    db.run("UPDATE todo SET state = 'Done' WHERE todo = ?", [text]);
+    dbAPI.run("UPDATE todo SET state = 'Done' WHERE todo = ?", [text]);
     
-    // Boilerplate: render the table
     renderTable();
+}
+
+function renderTable() {
+  // execute select
+  const result = dbAPI.select("SELECT * FROM todo");
+  // construct table
+  // base case - no data
+  if (result.length === 0) {
+    document.getElementById("sql-result").innerHTML = "";
+    return;
+  }
+
+  // construct the header
+  let headerString = "";
+  result[0].columns.forEach((column) => {
+    headerString += `<th>${column}</th>`;
+  });
+  headerString += `<th>Actions</th>`;
+  headerString = `<tr>${headerString}</tr>`;
+
+  // construct the rows
+  var rows = "";
+  result[0].values.forEach((row) => {
+    var rowString = "";
+    row.forEach((column) => {
+      rowString += `<td>${column}</td>`;
+    });
+    rowString += `<td><button class="btn btn-primary" onclick="deleteTODO(${row[0]})">Delete</button></td>`;
+    rowString = `<tr>${rowString}</tr>`;
+    rows += rowString;
+  });
+
+  // render the table
+  let tableString = `<table>${headerString}${rows}</table>`;
+
+  // insert the tableString into the table
+  document.getElementById("sql-result").innerHTML = tableString;
 }
